f04d99835dfb788784bc2b1c106f677e7807580a
suKyoung Shin <sukyoung.dev@gmail.com>
2023-05-29 20:54:19 +0900

    fix: 로직수정 & 그에 따른 컴파일된 Js파일 업로드

--- built/index.js
+++ built/index.js
@@ -1,57 +1,150 @@
 import { vocabularies } from "./data.js";
 const init = () => {
     getVocabularyArray();
+    handlePrint();
+    handleSort();
+    handleAnswer();
 };
 window.addEventListener("load", init);
 const getHTMLElement = () => {
     const $testWrapper = document.querySelector(".test-wrapper");
-    return { $testWrapper };
+    const $answer = document.querySelectorAll('.table-cell:nth-child(3n)');
+    const $button = document.querySelectorAll("button");
+    return { $testWrapper, $button, $answer };
 };
-const getVocabularyArray = () => {
-    const vocabularyData = [];
-    vocabularies.map((v, idx) => {
-        const result = { id: 0, vocabulary: '', meaning: '' };
-        result.id = idx + 1;
-        result.vocabulary = v.vocabulary;
-        result.meaning = v.meaning;
-        vocabularyData.push(result);
-    });
-    const removePartialData = [...vocabularyData];
-    removePartialData.map((t, idx) => idx < 10 && (t.vocabulary = ""));
-    removePartialData.map((t, idx) => idx >= 10 && (t.meaning = ""));
+/** vocabulary */
+const getVocabularyData = (sort) => {
+    let vocabularyData = getDailyVocabularyData();
+    if (sort === "acend") {
+        vocabularyData = getAscendingVocabularyData();
+    }
+    else if (sort === "desc") {
+        vocabularyData = getDescendingVocabularyData();
+    }
+    return vocabularyData;
+};
+const getVocabularyArray = (sort) => {
+    const vocabularyData = getVocabularyData(sort);
     getVocabularyItem(vocabularyData);
+};
+const getDailyVocabularyData = () => {
+    return vocabularies
+        .map((v, idx) => (Object.assign(Object.assign({}, v), { id: 1 + idx })));
+};
+const getAscendingVocabularyData = () => {
+    return [...vocabularies]
+        .sort((a, b) => a.vocabulary.localeCompare(b.vocabulary))
+        .map((v, idx) => (Object.assign(Object.assign({}, v), { id: 1 + idx })));
+};
+const getDescendingVocabularyData = () => {
+    return [...vocabularies]
+        .sort((a, b) => b.vocabulary.localeCompare(a.vocabulary))
+        .map((v, idx) => (Object.assign(Object.assign({}, v), { id: 1 + idx })));
+};
+const createTitleElement = ($testWrapper) => {
+    const $testTitle = document.createElement('div');
+    $testTitle.setAttribute('class', 'table-row table-subtitle');
+    const $no = document.createElement('div');
+    $no.innerText = "No";
+    $testTitle.append($no);
+    const $words = document.createElement('div');
+    $words.innerText = "Words";
+    $testTitle.append($words);
+    const $meaning = document.createElement('div');
+    $meaning.innerText = "Meaning";
+    $testTitle.append($meaning);
+    $testWrapper.append($testTitle);
 };
 const getVocabularyItem = (vocabularies) => {
     const { $testWrapper } = getHTMLElement();
     if (!$testWrapper)
         return;
-    vocabularies.map((vocabulary) => {
+    // 기존 Element 삭제
+    $testWrapper.innerHTML = '';
+    // 제목 생성 (No, Words, Meaning)
+    createTitleElement($testWrapper);
+    // 컨텐츠 생성 (단어, 뜻)
+    vocabularies.forEach((v) => {
+        const { id, vocabulary, meaning } = v;
         const $tableRow = document.createElement("div");
         $tableRow.setAttribute("class", "table-row");
-        if (vocabulary.id !== 0) {
-            const $number = document.createElement("div");
-            $number.setAttribute("class", "table-cell");
-            $number.innerText = String(vocabulary.id);
-            $tableRow.append($number);
+        if (id) {
+            const $element = createTableCellElement(id);
+            $tableRow.append($element);
         }
-        if (vocabulary.vocabulary !== "") {
-            const $vocabulary = document.createElement("div");
-            $vocabulary.setAttribute("class", "table-cell");
-            $vocabulary.innerText = vocabulary.vocabulary;
-            $tableRow.append($vocabulary);
+        else {
+            const $element = createTableCellElement("");
+            $tableRow.append($element);
         }
-        if (vocabulary.meaning !== "") {
-            const $meaning = document.createElement("div");
-            $meaning.setAttribute("class", "table-cell");
-            $meaning.innerText = vocabulary.meaning;
-            $tableRow.append($meaning);
+        if (vocabulary) {
+            const $element = createTableCellElement(vocabulary);
+            $tableRow.append($element);
         }
-        if (vocabulary.id === 0 || vocabulary.vocabulary === "" || vocabulary.meaning === "") {
-            const $empty = document.createElement("div");
-            $empty.setAttribute("class", "table-cell");
-            $empty.innerText = "";
-            $tableRow.append($empty);
+        else {
+            const $element = createTableCellElement("");
+            $tableRow.append($element);
+        }
+        if (meaning) {
+            const $element = createTableCellElement(meaning);
+            $tableRow.append($element);
+        }
+        else {
+            const $element = createTableCellElement("");
+            $tableRow.append($element);
         }
         $testWrapper.append($tableRow);
     });
+};
+const createTableCellElement = (key) => {
+    const $element = document.createElement("div");
+    $element.setAttribute("class", "table-cell");
+    $element.innerText = `${key}`;
+    return $element;
+};
+/** event */
+const handlePrint = () => {
+    const { $button } = getHTMLElement();
+    if (!$button)
+        return;
+    for (let i = 0; i < $button.length; i++) {
+        if ($button[i].id === "print") {
+            $button[i].addEventListener('click', print);
+        }
+    }
+};
+const print = () => {
+    return window.print();
+};
+const handleSort = () => {
+    const { $button, $testWrapper } = getHTMLElement();
+    if (!$button || !$testWrapper)
+        return;
+    for (let i = 0; i < $button.length; i++) {
+        if ($button[i].id === "acend" || $button[i].id === "desc" || $button[i].id === "origin") {
+            $button[i].addEventListener('click', () => getVocabularyArray($button[i].id));
+        }
+    }
+};
+const handleAnswer = () => {
+    const { $button } = getHTMLElement();
+    if (!$button)
+        return;
+    for (let i = 0; i < $button.length; i++) {
+        if ($button[i].id === "show" || $button[i].id === "hide") {
+            $button[i].addEventListener('click', () => toggleAnswer($button[i].id));
+        }
+    }
+};
+const toggleAnswer = (id) => {
+    const { $answer } = getHTMLElement();
+    for (let i = 0; i < $answer.length; i++) {
+        if (id === "show") {
+            $answer[i].classList.remove('hide');
+            $answer[i].classList.add('show');
+        }
+        else {
+            $answer[i].classList.remove('show');
+            $answer[i].classList.add('hide');
+        }
+    }
 };
